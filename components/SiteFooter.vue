21c1358717f61f5a5b894d6ce8473cf8b5548bbd
Giuseppe Frattura <giuseppefrattura@gmail.com>
2023-07-05 22:46:21 +0200

    added header footer and homepage

---
+++ components/SiteFooter.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const links =[
+        { name:'Github' , href:'http://github.com', icon: 'entypo-social:github'},
+        { name:'500px' , href:'http://500px.com', icon: 'entypo-social:500px'},
+        { name:'Instagram' , href:'http://instagram.com', icon: 'entypo-social:instagram'},
+        { name:'Linkedin' , href:'http://linkedin.com', icon: 'entypo-social:linkedin'},
+        { name:'Medium' , href:'http://medium.com', icon: 'entypo-social:medium'},
+        { name:'Twitter' , href:'http://twitter.com', icon: 'entypo-social:twitter'},
+    ]
+</script>
+<template>
+    <footer class="bg-gray-800">
+        <section class="mx-auto max-w-7xl px2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
+            <p class="flex-1">Giuseppe Frattura 2023</p>
+            <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
+                <a v-for="link in links" :key="link.name" :href="link.href">
+                    <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
+                </a>
+            </div>
+        </section>
+
+    </footer>
+        
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">Ciao, sono Giuseppe Frattura</h1>
+        <p class="text-base p-2 italic">DevOps Engineer in MDOTM</p>
+    </section>
+    <section class="flex flex-col " >
+        <h2 class="text-3xl font-bold mt-8">La mia storia</h2>
+        <p>PEPepepepeksfjhbdksjnfkjsnflksdl</p>
+    </section>
+</template>
